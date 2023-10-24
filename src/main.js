a626d3caa332e543f63d591b420dfce0b55545b0
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2023-10-24 14:13:49 +0900

    map and Character load

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// import * as THREE from '../THREE.js'
-
 const _VS = `
 varying vec3 vWorldPosition;
 
@@ -9,7 +7,6 @@ void main() {
 
   gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 }`;
-
 
 const _FS = `
 uniform vec3 topColor;
@@ -24,101 +21,125 @@ void main() {
   gl_FragColor = vec4( mix( bottomColor, topColor, max( pow( max( h , 0.0), exponent ), 0.0 ) ), 1.0 );
 }`;
 
-class AnimalCrossing{
-    Initialize() {
-        this._threejs = new THREE.WebGLRenderer({
-          antialias: true,
-        });
-        this._threejs.outputEncoding = THREE.sRGBEncoding;
-        this._threejs.gammaFactor = 2.2;
-        this._threejs.shadowMap.enabled = true;
-        this._threejs.shadowMap.type = THREE.PCFSoftShadowMap;
-        this._threejs.setPixelRatio(window.devicePixelRatio);
-        this._threejs.setSize(window.innerWidth, window.innerHeight);
-        this._threejs.domElement.id = 'threejs';
-    
-        document.getElementById('container').appendChild(this._threejs.domElement);
-    
-        window.addEventListener('resize', () => {
-          this._OnWindowResize();
-        }, false);
-    
-        const fov = 60;
-        const aspect = 1920 / 1080;
-        const near = 1.0;
-        const far = 10000.0;
-        this._camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-        this._camera.position.set(25, 10, 25);
-    
-        this._scene = new THREE.Scene();
-        this._scene.background = new THREE.Color(0xFFFFFF);
-        this._scene.fog = new THREE.FogExp2(0x89b2eb, 0.002);
-    
-        let light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
-        light.position.set(-10, 500, 10);
-        light.target.position.set(0, 0, 0);
-        light.castShadow = true;
-        light.shadow.bias = -0.001;
-        light.shadow.mapSize.width = 4096;
-        light.shadow.mapSize.height = 4096;
-        light.shadow.camera.near = 0.1;
-        light.shadow.camera.far = 1000.0;
-        light.shadow.camera.left = 100;
-        light.shadow.camera.right = -100;
-        light.shadow.camera.top = 100;
-        light.shadow.camera.bottom = -100;
-        this._scene.add(light);
-    
-        this._sun = light;
-    
-        const plane = new THREE.Mesh(
-            new THREE.PlaneGeometry(5000, 5000, 10, 10),
-            new THREE.MeshStandardMaterial({
-                color: 0x1e601c,
-              }));
-        plane.castShadow = false;
-        plane.receiveShadow = true;
-        plane.rotation.x = -Math.PI / 2;
-        this._scene.add(plane);
-    
-        this._entityManager = new entity_manager.EntityManager();
-        this._grid = new spatial_hash_grid.SpatialHashGrid(
-            [[-1000, -1000], [1000, 1000]], [100, 100]);
-    
-        this._LoadControllers();
-        this._LoadPlayer();
-        this._LoadFoliage();
-        this._LoadClouds();
-        this._LoadSky();
-    
-        this._previousRAF = null;
-        this._RAF();
-    }
-    _LoadSky() {
-        const hemiLight = new THREE.HemisphereLight(0xFFFFFF, 0xFFFFFFF, 0.6);
-        hemiLight.color.setHSL(0.6, 1, 0.6);
-        hemiLight.groundColor.setHSL(0.095, 1, 0.75);
-        this._scene.add(hemiLight);
+class AnimalCrossing {
+  constructor() {
+    this._Initialize();
+  }
 
-        const uniforms = {
-            "topColor": { value: new THREE.Color(0x0077ff) },
-            "bottomColor": { value: new THREE.Color(0xffffff) },
-            "offset": { value: 33 },
-            "exponent": { value: 0.6 }
-        };
-        uniforms["topColor"].value.copy(hemiLight.color);
+  _Initialize() {
+    this._threejs = new THREE.WebGLRenderer({
+      antialias: true,
+    });
+    this._threejs.outputEncoding = THREE.sRGBEncoding;
+    this._threejs.gammaFactor = 2.2;
+    this._threejs.shadowMap.enabled = true;
+    this._threejs.shadowMap.type = THREE.PCFSoftShadowMap;
+    this._threejs.setPixelRatio(window.devicePixelRatio);
+    this._threejs.setSize(window.innerWidth, window.innerHeight);
+    this._threejs.domElement.id = 'threejs';
 
-        this._scene.fog.color.copy(uniforms["bottomColor"].value);
+    document.getElementById('container').appendChild(this._threejs.domElement);
 
-        const skyGeo = new THREE.SphereBufferGeometry(1000, 32, 15);
-        const skyMat = new THREE.ShaderMaterial({
-            uniforms: uniforms,
-            vertexShader: _VS,
-            fragmentShader: _FS,
-            side: THREE.BackSide
-        });
+    window.addEventListener('resize', () => {
+      this._OnWindowResize();
+    }, false);
 
-        const sky = new THREE.Mesh(skyGeo, skyMat);
-        this._scene.add(sky);
-    }
+
+    const fov = 60;
+    const aspect = 1920 / 1080;
+    const near = 1.0;
+    const far = 10000.0;
+    this._camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    this._camera.position.set(25, 10, 25);
+
+    this._scene = new THREE.Scene();
+    this._scene.background = new THREE.Color(0xFFFFFF);
+    this._scene.fog = new THREE.FogExp2(0x89b2eb, 0.002);
+
+    const hemiLight = new THREE.HemisphereLight(0xFFFFFF, 0xFFFFFFF, 0.6);
+    hemiLight.color.setHSL(0.6, 1, 0.6);
+    hemiLight.groundColor.setHSL(0.095, 1, 0.75);
+    this._scene.add(hemiLight);
+
+    const uniforms = {
+      "topColor": { value: new THREE.Color(0x0077ff) },
+      "bottomColor": { value: new THREE.Color(0xffffff) },
+      "offset": { value: 33 },
+      "exponent": { value: 0.6 }
+    };
+    uniforms["topColor"].value.copy(hemiLight.color);
+
+    this._scene.fog.color.copy(uniforms["bottomColor"].value);
+
+    const skyGeo = new THREE.SphereBufferGeometry(1000, 32, 15);
+    const skyMat = new THREE.ShaderMaterial({
+      uniforms: uniforms,
+      vertexShader: _VS,
+      fragmentShader: _FS,
+      side: THREE.BackSide
+    });
+
+    const sky = new THREE.Mesh(skyGeo, skyMat);
+    this._scene.add(sky);
+
+    this._previousRAF = null;
+    this._RAF();
+
+	this._loadMap();
+	this._LoadCharacterModel(); 
+  }
+
+  _OnWindowResize() {
+    this._camera.aspect = window.innerWidth / window.innerHeight;
+    this._camera.updateProjectionMatrix();
+    this._threejs.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  _RAF() {
+    requestAnimationFrame((t) => {
+      if (this._previousRAF === null) {
+        this._previousRAF = t;
+      }
+
+      this._RAF();
+
+      this._threejs.render(this._scene, this._camera);
+    //   this._Step(t - this._previousRAF);
+      this._previousRAF = t;
+    });
+  }
+  _loadMap() {
+    const loader = new THREE.GLTFLoader();
+
+    // Load the GLTF model
+    loader.load('resources/animal_crossing_map/scene.gltf', (gltf) => {
+      const model = gltf.scene;
+
+      // Adjust the position, scale, or rotation as needed
+      model.position.set(0, 0, 0);
+      model.scale.set(100, 100, 100);
+      model.rotation.set(0, 0, 0);
+
+      // Add the model to the scene
+      this._scene.add(model);
+    });
+  }
+  _LoadCharacterModel() {
+    const loader = new THREE.GLTFLoader();
+
+    // Load the character GLTF model
+    loader.load('resources/marshal_from_animal_crossing/scene.gltf', (gltf) => {
+      const character = gltf.scene;
+
+      // Adjust the position to place the character above the map
+      character.position.set(30, 1, 0); // Modify the position as needed
+
+      // Add the character to the scene
+      this._scene.add(character);
+
+    });
+  }
+
 }
+
+const animalCrossing = new AnimalCrossing();

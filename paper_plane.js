4743b65f3d574f8b6a59be5e0e11a9538f17a371
joonyeoblee <101649294+joonyeoblee@users.noreply.github.com>
2023-11-05 20:39:10 +0900

    Update paper_plane.js
    클릭시 화면전환

--- paper_plane.js
+++ paper_plane.js
@@ -144,6 +144,15 @@ window.onload = function init() {
 				}
 			}, 23);
 
+			if (event.buttons == 1 && plane) {
+				// ...
+			
+				// 5초 후에 페이지 이동
+				setTimeout(function() {
+				  window.location.href = "./index.html"; // 새로운 페이지로 이동
+				}, 5000); // 5000 밀리초 (5초) 지연
+			}
+
 		}
 
 

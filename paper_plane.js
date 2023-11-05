1b1020433a8108333651f64b7425aea3d7c5b0f5
2sojeong <dlgur200123@naver.com>
2023-11-05 16:51:40 +0900

    Update paper_plane.js

--- paper_plane.js
+++ paper_plane.js
@@ -1,6 +1,5 @@
-let canvas; // 캔버스 엘리먼트를 변수로 선언합니다.
-let context; // 캔버스 컨텍스트를 변수로 선언합니다.
 
+let hidingText = false;
 window.onload = function init() {
 
 	const canvas = document.getElementById("gl-canvas");
@@ -86,30 +85,32 @@ window.onload = function init() {
 	function handleMouseMove(event) {
 		event.preventDefault();
 
-		// 마우스로 클릭한 상태인 경우 비행기의 위치를 변경
 		if (event.buttons == 1 && plane) {
 			const canvas = document.createElement("canvas");
+			canvas.width = 256;
+			canvas.height = 256;
+
 			const context = canvas.getContext("2d");
-			context.clearRect(0, 0, canvas.width, canvas.height);
+			if (!hidingText) {
+				// Set a transparent color to hide the text
+				context.fillStyle = "rgba(255, 255, 255, 255)";
+				context.fillRect(0, 0, canvas.width, canvas.height);
+				hidingText = true;
+				console.log(hidingText);
+			}
+
 			const interval1StartTime = Date.now();
 
 			const interval1 = setInterval(() => {
-				// 비행기를 왼쪽으로 빠르게 이동시킴
 				plane.position.x -= planeSpeed * 0.3;
 
-				//console.log(plane.position.y);
-
-				// 화면 밖으로 나가는 조건을 설정할 수 있음
-				// if (plane.position.x < minX) { plane.position.x = minX; }
-
-				// plane.position.x가 원하는 위치에 도달하면 interval을 종료
 				if (plane.position.x <= -37) {
 					clearInterval(interval1);
 					const interval1EndTime = Date.now() + 800;
 					const interval1Duration = interval1EndTime - interval1StartTime;
 					setTimeout(startSecondInterval, interval1Duration);
 				}
-			}, 23); // 0.2초(200ms) 간격으로 이동
+			}, 23);
 		}
 	}
 

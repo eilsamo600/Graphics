0140955b4b5ac47e052cc5fbb65ba2d312c37c75
bamb14 <118671229+bamb14@users.noreply.github.com>
2023-11-05 17:15:27 +0900

    Update paper_plane.js

--- paper_plane.js
+++ paper_plane.js
@@ -115,12 +115,17 @@ window.onload = function init() {
 
 	function handleMouseMove(event) {
 		event.preventDefault();
-		if (event.buttons == 1 && plane) {
-			sound.play();
-			isSoundPlaying = false;
-		}
+
+
 
 		if (event.buttons == 1 && plane) {
+
+			sound.play();
+			isSoundPlaying = false;
+
+			// 마우스 이벤트 리스너 제거
+			window.removeEventListener('mousemove', handleMouseMove);
+
 			const canvas = document.createElement("canvas");
 			canvas.width = 256;
 			canvas.height = 256;
@@ -167,27 +172,22 @@ window.onload = function init() {
 
 		const interval2 = setInterval(() => {
 
-			if (plane.position.x == -27) {
-
+			if (isSoundPlaying) {
 				sound.play();
-				isSoundPlaying = false;
+				isSoundPlaying = false; // 소리를 한 번만 재생하도록 변수 값을 변경합니다.
 			}
 
 			plane.position.x += planeSpeed * 0.3;
 			plane.position.y += planeSpeed * 0.02;
 			plane.position.z -= planeSpeed * 0.2;
 
-
 			//console.log(plane.position.x);
 
-			// plane.position.x가 원하는 위치에 도달하면 interval을 종료
 			if (plane.position.x >= 120) {
 				clearInterval(interval2);
 			}
 		}, 23);
 
-		// 마우스 이벤트 리스너 제거
-		window.removeEventListener('mousemove', handleMouseMove);
 	}
 
 
